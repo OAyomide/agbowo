115797db33dbfa9d6c8afead8a9681f537f1e1df
Ayomide Onigbinde <onigbindeayomide@gmail.com>
2019-06-13 21:28:08 +0100

    [wip] feat.
    get all the user's messages. currently retrieving for only gtbank for now

--- android/app/src/main/java/com/agbowo/MainApplication.java
+++ android/app/src/main/java/com/agbowo/MainApplication.java
@@ -3,6 +3,7 @@ package com.agbowo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.react.SmsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SmsPackage(),
             new RNGestureHandlerPackage(),
             new AsyncStoragePackage()
       );
